1b6634c3fefc9493f8e001b8c3a3c7ba69af39bf
Damien Ramelet <damien.ramelet@protonmail.com>
2025-01-14 15:20:47 +0100

    (fix) Don’t break tab insertion

--- lua/opts.lua
+++ lua/opts.lua
@@ -19,6 +19,11 @@ local function close_current_buffer()
 end
 
 
+-- So don't break tab insertion
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
 
 -- keyset("mode", "keys", "function-called")
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
